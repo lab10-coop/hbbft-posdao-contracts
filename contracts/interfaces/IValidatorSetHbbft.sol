6e52458221d3fe7c4e723bfae5e9c065d6663182
didi <git@d10r.net>
2020-10-08 18:22:48 +0200

    add method for getting pubKey with mining key as argument

--- contracts/interfaces/IValidatorSetHbbft.sol
+++ contracts/interfaces/IValidatorSetHbbft.sol
@@ -29,5 +29,6 @@ interface IValidatorSetHbbft {
     function reportMaliciousCallable(address, address, uint256) external view returns(bool, bool);
     function stakingByMiningAddress(address) external view returns(address);
     function publicKeyByStakingAddress(address) external view returns(bytes memory);
+    function getPublicKey(address) external view returns(bytes memory);
     function stakingContract() external view returns(address);
 }
